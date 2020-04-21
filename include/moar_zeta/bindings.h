f1138eab24c23c3dbd135da199ca1212c96e6a81
Adam Zlatniczki <adam.zlatniczki@cs.bme.hu>
2020-04-21 04:09:31 +0200

    Added R bindings

--- include/moar_zeta/bindings.h
+++ include/moar_zeta/bindings.h
@@ -17,4 +17,20 @@ extern "C" {
      */
     void hmp_value_py(const double* x, const double* y, const unsigned int d_x, const unsigned int d_y, const unsigned int n, double& hmp_x, double& avg_zeta_x, double& hmp_y, double& avg_zeta_y, const unsigned int n_tests=50, unsigned int k=0);
 
+    /**
+     * @brief Main function, calculates the zeta test statistics and their corresponding (harmonic mean) p-values.
+     *
+     * @param x The X sample points, vectorized.
+     * @param y The Y sample points, vectorized.
+     * @param d_x The algebraic dimension of the X sample points.
+     * @param d_y The algebraic dimension of the Y sample points.
+     * @param n The number of points.
+     * @param hmp_x Reference where the HMP of Zeta_X will be exported.
+     * @param avg_zeta_x Reference where the test statistic Zeta_X will be exported.
+     * @param hmp_y Reference where the HMP of Zeta_Y will be exported.
+     * @param avg_zeta_y Reference where the test statistic Zeta_Y will be exported.
+     * @param n_tests Number of tests to do (number of random subsamples, or L in the HMP terminology).
+     * @param k Number of nearest neighbors (excluding self) to calculate. If not set, ceil(sqrt(n)).
+     */
+    void hmp_value_r(const double* x, const double* y, const unsigned int* d_x, const unsigned int* d_y, const unsigned int* n, double* hmp_x, double* avg_zeta_x, double* hmp_y, double* avg_zeta_y, const unsigned int* n_tests, const unsigned int* k);
 }
--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,6 +1,11 @@
-#include "moar_zeta.h"
-#include "bindings.h" //this is required for the function implementation to "know" that it's extern C
+#include "moar_zeta/moar_zeta.h"
+#include "moar_zeta/bindings.h" //this is required for the function implementation to "know" that it's extern C
+
 
 void hmp_value_py(const double* x, const double* y, const unsigned int d_x, const unsigned int d_y, const unsigned int n, double& hmp_x, double& avg_zeta_x, double& hmp_y, double& avg_zeta_y, const unsigned int n_tests, unsigned int k) {
     hmp_value(x, y, d_x, d_y, n, hmp_x, avg_zeta_x, hmp_y, avg_zeta_y, n_tests, k);
+};
+
+void hmp_value_r(const double* x, const double* y, const unsigned int* d_x, const unsigned int* d_y, const unsigned int* n, double* hmp_x, double* avg_zeta_x, double* hmp_y, double* avg_zeta_y, const unsigned int* n_tests, const unsigned int* k) {
+    hmp_value(x, y, *d_x, *d_y, *n, *hmp_x, *avg_zeta_x, *hmp_y, *avg_zeta_y, *n_tests, *k);
 };
