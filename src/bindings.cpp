cb78832bb52aa1b3979e9f55c4409458ce85fef7
Adam Zlatniczki <adam.zlatniczki@cs.bme.hu>
2020-05-27 08:19:26 +0200

    Added explanatory comment to R bindings

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -7,5 +7,6 @@ void hmp_value_py(const double* x, const double* y, const unsigned int d_x, cons
 };
 
 void hmp_value_r(const double* x, const double* y, const unsigned int* d_x, const unsigned int* d_y, const unsigned int* n, double* hmp_x, double* avg_zeta_x, double* hmp_y, double* avg_zeta_y, const unsigned int* n_tests, const unsigned int* k) {
+    // R passes all parameters as a pointer, so the binding must take care of this
     hmp_value(x, y, *d_x, *d_y, *n, *hmp_x, *avg_zeta_x, *hmp_y, *avg_zeta_y, *n_tests, *k);
 };
